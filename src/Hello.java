774f5b1b7d1e050662933a3cda69d2f4a972b3db
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-06-28 22:54:07 -0500

    more java practice

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,16 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Rachel");
 
-        int myFirstNumber = (5 + 5) + (2 * 10);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
         System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+
     }
 }
 
