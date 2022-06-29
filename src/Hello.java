4fab12d4d8c9fe21c8708f9ed97f96babc5ad4f8
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-06-28 22:42:58 -0500

    intro java udemy

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Rachel");
+
+        int myFirstNumber = (5 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+    }
+}
+
+
