32eb09fc032bec40a79168015b8ad1443c655471
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-07-25 20:04:12 -0500

    more on floats and doubles

--- src/academy/learnprogramming/floatsAndDoubles.java
+++ src/academy/learnprogramming/floatsAndDoubles.java
@@ -13,10 +13,22 @@ public class floatsAndDoubles {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5/3;
+        float myFloatValue = 5f /3f;
+        double myDoubleValue = 5.00 /3.00;
+        System.out.println("My int value = " + myIntValue);
+        System.out.println("My float value = " + myFloatValue);
+        System.out.println("My double value = " + myDoubleValue);
 
+        double myPounds = 5d;
+        double myKilos = myPounds * 0.45359237d;
+        System.out.println(myPounds + "lbs converted to kilos is = " + myKilos);
+
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 
 }
