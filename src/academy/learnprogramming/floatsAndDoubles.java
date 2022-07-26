cb4335d01af7ee9ab3f45114439bac940585407e
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-07-25 19:46:54 -0500

    floats and doubles

--- src/academy/learnprogramming/floatsAndDoubles.java
+++ src/academy/learnprogramming/floatsAndDoubles.java
@@ -12,6 +12,11 @@ public class floatsAndDoubles {
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
     }
 
 }
